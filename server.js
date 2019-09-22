dcc521f180beb64be737bd936fd044482ce64043
BigBrom <44329168+BigBrom@users.noreply.github.com>
2019-09-22 14:32:31 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const port = 3000
+const app = express()
+
+app.use(cookieParser())
+
+app.get('/', function (req, res) {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+    console.log('ip: ', ip);
+    
+    // Cookies that have not been signed
+    console.log('Cookies: ', req.cookies)
+    // Cookies that have been signed
+    console.log('Signed Cookies: ', req.signedCookies)
+    res.cookie('cookieName','hello world', { maxAge: 900000, httpOnly: true })
+    res.send('hello world')
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
